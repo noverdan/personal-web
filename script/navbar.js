5c435de06ec0777cec7bb1dd1aef751e172a66a4
Galang Arsandy <galangarsandy@gmail.com>
2023-10-08 02:39:57 +0700

    Menambahkan page about, projects, blog dan index

---
+++ script/navbar.js
@@ -0,0 +1,7 @@
+let menuToggle = document.querySelector(".toggle");
+let navMenu = document.querySelector(".navbar-menu");
+
+menuToggle.addEventListener('click', function() {
+    navMenu.classList.toggle('active');
+    
+});
